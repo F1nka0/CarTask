e85d4f7ea7a55baa088ae6b72ffed9b31353f20f
F1nk40 <riainar@bk.ru>
2022-02-25 20:41:24 +0300

    failure validation has been added

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace WpfApp1
     public partial class MainWindow : Window
     {
         static List<Car> Cars = new List<Car>();
-        static List<string> BasicStringValuesForTextBoxes = new List<string>(new string[] { "Номер", "Марка", "Заполните поле", " ", "", "Путь" });
+        static List<string> BasicStringValuesForTextBoxes = new List<string>(new string[] { "Неисправность","Номер", "Марка", "Заполните поле", " ", "", "Путь" });
         public MainWindow()
         {
             InitializeComponent();
@@ -42,6 +42,11 @@ namespace WpfApp1
             if (new Regex(@"^([a-z]+|[а-я]+)$").IsMatch(Mark.Text)==false)
             {
                 Mark.Text = "Марка введена некорректно";
+                return;
+            }
+            if (new Regex(@"^[0-9]+$").IsMatch(Failure.Text) == false)
+            {
+                Failure.Text = "Неисправность введена некорректно";
                 return;
             }
             if (IsFieldFilled(Number) == false | IsFieldFilled(Mark) == false)
@@ -99,23 +104,25 @@ namespace WpfApp1
                 Mark.Text = "Марка";
             }
         }
-        private bool AreAllInputsValid(string entireString, string number,string mark) {
-
-
-            return new Regex(@"[а-я]\d{3}[а-я]{2}").IsMatch(number) && new Regex(@"([a-z]|[а-я])+").IsMatch(mark)&& entireString.Count(it=>it=='@') == 2;
+        private bool AreAllInputsValid(string entireString, string number,string mark,string failure) {
+            return new Regex(@"[а-я]\d{3}[а-я]{2}").IsMatch(number) && new Regex(@"([a-z]|[а-я])+").IsMatch(mark)&& entireString.Count(it=>it=='@') == 2&& new Regex(@"^[0-9]+$").IsMatch(failure);
         }
         private void GetFile_Click(object sender, RoutedEventArgs e)
         {
             try
             {
+                List<Car> tempCarList = new List<Car>();
                 string[] CurrentCar;
                 StreamReader reader = new StreamReader(File.Open(XMLPath.Text,FileMode.Open,FileAccess.Read,FileShare.Read));
                 foreach (string str in reader.ReadToEnd().Split('|')) {
                     CurrentCar = str.Split('@');
-                    if ((AreAllInputsValid(str,CurrentCar.FirstOrDefault(), CurrentCar.LastOrDefault())==false)) { XMLPath.Text = "Файл не валиден"; return; }
-                    Cars.Add(new Car(CurrentCar[0],CurrentCar[1],CurrentCar[2]));
+                    if ((AreAllInputsValid(str,CurrentCar.FirstOrDefault(), CurrentCar.LastOrDefault(), CurrentCar.Skip(1).FirstOrDefault()) ==false)) { XMLPath.Text = "Файл не валиден"; return; }
+                    tempCarList.Add(new Car(CurrentCar[0],CurrentCar[1].Aggregate("",(a,b)=>a+" "+b),CurrentCar[2]));
                 }
-                dg.Items.Refresh();  
+                Cars.AddRange(tempCarList);
+                tempCarList.Clear();
+                dg.Items.Refresh();
+                reader.Close();
             }
             catch (Exception)
             {
@@ -139,6 +146,22 @@ namespace WpfApp1
                 XMLPath.Text = "Путь";
             }
             
+        }
+
+        private void Failure_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (BasicStringValuesForTextBoxes.Any(it => Failure.Text == it))
+            {
+                Failure.Text = "";
+            }
+        }
+
+        private void Failure_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (BasicStringValuesForTextBoxes.Any(it => Failure.Text == it))
+            {
+                Failure.Text = "Неисправность";
+            }
         }
     }
 
